9da8b9bfe557c346722c787fc0b352fa2aabea15
Joseph C. Osborn <joseph.osborn@pomona.edu>
2020-11-11 14:06:44 -0800

    Draft output of linked maps

--- src/bin/batch.rs
+++ src/bin/batch.rs
@@ -46,6 +46,7 @@ fn main() {
         all_inputs,
         start.elapsed().as_secs_f64() / (all_inputs as f64)
     );
-    println!("{}",mappy.timers);
-    // mappy.dump_tiles(Path::new("out/"));
+    println!("{}", mappy.timers);
+    mappy.dump_map(Path::new("out/map.dot"));
+    // mappy.dump_tiles(Path::new("out/tiles"));
 }
